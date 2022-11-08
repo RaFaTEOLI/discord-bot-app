35d5af443ab011ba2b52dbbf2f7b56d2f849e7e2
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-08 13:56:24 +0100

    feat: add local storage adapter factory

---
+++ src/main/factories/cache/local-storage-adapter-factory.ts
@@ -0,0 +1,5 @@
+import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter';
+
+export const makeLocalStorageAdapter = (): LocalStorageAdapter => {
+  return new LocalStorageAdapter();
+};
