0c0bf2e23266b9d502dfa7c02cd88947a3507d62
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-08 13:54:05 +0100

    feat: add local storage adapter

---
+++ src/infra/cache/local-storage-adapter.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { SetStorage } from '@/data/protocols/cache';
+
+export class LocalStorageAdapter implements SetStorage {
+  set(key: string, value: object | null): void {
+    if (value) {
+      localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      localStorage.removeItem(key);
+    }
+  }
+
+  get(key: string): any {
+    // @ts-ignore
+    return JSON.parse(localStorage.getItem(key));
+  }
+}
