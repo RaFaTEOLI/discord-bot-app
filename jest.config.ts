ee95b8c703627e4c6f2a09f8807f440f852582a9
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-08 13:50:27 +0100

    chore: setup project config

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,23 @@
-module.exports = {
+export default {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**/*',
     '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/*/mocks/*',
+    '!<rootDir>/src/data/protocols/*',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/presentation/components/index.tsx',
     '!<rootDir>/src/presentation/pages/index.tsx',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
   testEnvironment: 'jsdom',
-  transform: {
-    '.+\\.(ts|tsx)$': 'ts-jest'
-  },
   moduleNameMapper: {
+    '\\.(css|sass|scss)$': 'identity-obj-proxy',
     '@/(.*)': '<rootDir>/src/$1'
   }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import EnvironmentPlugin from 'vite-plugin-environment';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   },
-  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
+  plugins: [react(), EnvironmentPlugin('all')]
 });
