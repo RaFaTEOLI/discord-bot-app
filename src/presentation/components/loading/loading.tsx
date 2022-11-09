22e84d4a831e407ed856fc7e61ddfe27e70099ef
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-09 10:55:52 +0100

    fix: add return type

--- src/presentation/components/loading/loading.tsx
+++ src/presentation/components/loading/loading.tsx
@@ -1,7 +1,7 @@
 import { Flex, Text } from '@chakra-ui/react';
 import Spinner from '../spinner/spinner';
 
-const Loading = () => (
+const Loading = (): JSX.Element => (
   <Flex data-testid="loading">
     <Text>Please Wait...</Text>
     <Spinner />
