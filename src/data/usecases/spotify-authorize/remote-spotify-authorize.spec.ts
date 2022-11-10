7cc4a9fe22b36b33fda301b4ca5f2def268dd051
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-10 14:22:09 +0100

    test: ensure RemoteSpotifyAuthorize will generate correct url

---
+++ src/data/usecases/spotify-authorize/remote-spotify-authorize.spec.ts
@@ -0,0 +1,26 @@
+import { mockSpotifyAuthorizeParams } from '@/domain/mocks';
+import { faker } from '@faker-js/faker';
+import { RemoteSpotifyAuthorize } from './remote-spotify-authorize';
+
+type SutTypes = {
+  sut: RemoteSpotifyAuthorize;
+};
+
+const makeSut = (url: string = faker.internet.url(), spotifySettings = mockSpotifyAuthorizeParams()): SutTypes => {
+  const sut = new RemoteSpotifyAuthorize(url, spotifySettings);
+  return {
+    sut
+  };
+};
+
+describe('RemoteSpotifyAuthorize', () => {
+  test('should call return authorize url with correct values', async () => {
+    const url = faker.internet.url();
+    const spotifyAuthorizeParams = mockSpotifyAuthorizeParams();
+    const { sut } = makeSut(url, spotifyAuthorizeParams);
+    const generatedUrl = await sut.authorize();
+    expect(generatedUrl).toBe(
+      `${url}?response_type=${spotifyAuthorizeParams.responseType}&client_id=${spotifyAuthorizeParams.clientId}&scope=${spotifyAuthorizeParams.scope}&redirect_uri=${spotifyAuthorizeParams.redirectUri}&state=${spotifyAuthorizeParams.state}`
+    );
+  });
+});
---
+++ src/domain/mocks/mock-spotify-authorize.ts
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker';
+import { SpotifyAuthorize } from '@/domain/usecases';
+
+export const mockSpotifyAuthorizeParams = (): SpotifyAuthorize.Params => {
+  return {
+    responseType: 'code',
+    clientId: faker.datatype.uuid(),
+    scope: 'user-read-private user-read-email',
+    redirectUri: faker.internet.url(),
+    state: faker.datatype.uuid()
+  };
+};
+
+export class SpotifyAuthorizeSpy implements SpotifyAuthorize {
+  url = faker.internet.url();
+  spotifySettings = mockSpotifyAuthorizeParams();
+  callsCount = 0;
+
+  async authorize(): Promise<string> {
+    this.callsCount++;
+    const settings = this.spotifySettings;
+    return Promise.resolve(
+      `${this.url}?response_type=${settings.responseType}&client_id${settings.clientId}&scope=${settings.scope}&redirect_uri=${settings.redirectUri}&state=${settings.state}`
+    );
+  }
+}
