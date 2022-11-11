e14fdba8f08668d5475add86cc8950cacbb0f665
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-11 15:34:50 +0100

    refactor: add redirect uri as parameter to factory

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,5 +2,10 @@ import { Login } from '@/presentation/pages';
 import { makeRemoteAuthentication, makeRemoteSpotifyAuthorize } from '@/main/factories/usecases';
 
 export const LoginFactory = (): JSX.Element => {
-  return <Login authentication={makeRemoteAuthentication()} spotifyAuthorize={makeRemoteSpotifyAuthorize()} />;
+  return (
+    <Login
+      authentication={makeRemoteAuthentication()}
+      spotifyAuthorize={makeRemoteSpotifyAuthorize(process.env.VITE_SPOTIFY_LOGIN_REDIRECT_URI as string)}
+    />
+  );
 };
--- src/main/factories/usecases/spotify-authorize/remote-spotify-authorize-factory.ts
+++ src/main/factories/usecases/spotify-authorize/remote-spotify-authorize-factory.ts
@@ -1,11 +1,11 @@
 import { SpotifyAuthorize } from '@/domain/usecases';
 import { RemoteSpotifyAuthorize } from '@/data/usecases';
 
-export const makeRemoteSpotifyAuthorize = (): SpotifyAuthorize => {
+export const makeRemoteSpotifyAuthorize = (redirectUri: string): SpotifyAuthorize => {
   return new RemoteSpotifyAuthorize(process.env.VITE_SPOTIFY_AUTHORIZE_URL as string, {
     responseType: 'code',
     clientId: process.env.VITE_SPOTIFY_CLIENT_ID as string,
-    redirectUri: process.env.VITE_SPOTIFY_LOGIN_REDIRECT_URI as string,
+    redirectUri,
     scope: 'user-read-private playlist-read-private user-read-email user-library-read',
     state: Date.now().toString()
   });
