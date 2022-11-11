2f3b76513429d715c82caacdd2cece5c38a8b18f
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-11 13:43:44 +0100

    chore: remove react strict mode

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import Router from './routes/router';
 import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <Router />
-    </ChakraProvider>
-  </React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <Router />
+  </ChakraProvider>
 );
