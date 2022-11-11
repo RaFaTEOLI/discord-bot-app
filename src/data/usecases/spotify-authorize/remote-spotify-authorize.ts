6f7d6d6fd89268ea9cef70b54200f5c12dcd0899
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-11 14:33:43 +0100

    feat: ensure spotify authorize url will be created correctly

--- src/data/usecases/spotify-authorize/remote-spotify-authorize.ts
+++ src/data/usecases/spotify-authorize/remote-spotify-authorize.ts
@@ -4,7 +4,11 @@ export class RemoteSpotifyAuthorize implements SpotifyAuthorize {
   constructor(private readonly url: string, private readonly spotifySettings: SpotifyAuthorize.Params) {}
 
   async authorize(): Promise<string> {
-    const queryString = `?response_type=${this.spotifySettings.responseType}&client_id=${this.spotifySettings.clientId}&scope=${this.spotifySettings.scope}&redirect_uri=${this.spotifySettings.redirectUri}&state=${this.spotifySettings.state}`;
+    const queryString = `?response_type=${this.spotifySettings.responseType}&client_id=${
+      this.spotifySettings.clientId
+    }&scope=${encodeURIComponent(this.spotifySettings.scope)}&redirect_uri=${encodeURIComponent(
+      this.spotifySettings.redirectUri
+    )}&state=${this.spotifySettings.state}`;
     return await Promise.resolve(`${this.url}${queryString}`);
   }
 }
--- src/main/factories/usecases/spotify-authorize/remote-spotify-authorize-factory.ts
+++ src/main/factories/usecases/spotify-authorize/remote-spotify-authorize-factory.ts
@@ -5,7 +5,7 @@ export const makeRemoteSpotifyAuthorize = (): SpotifyAuthorize => {
   return new RemoteSpotifyAuthorize(process.env.VITE_SPOTIFY_AUTHORIZE_URL as string, {
     responseType: 'code',
     clientId: process.env.VITE_SPOTIFY_CLIENT_ID as string,
-    redirectUri: process.env.VITE_SPOTIFY_REDIRECT_URI as string,
+    redirectUri: process.env.VITE_SPOTIFY_LOGIN_REDIRECT_URI as string,
     scope: 'user-read-private playlist-read-private user-read-email user-library-read',
     state: Date.now().toString()
   });
