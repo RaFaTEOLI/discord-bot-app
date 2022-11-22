a85a4f89092f02ca2bac831ed17a67f29b1a6f0d
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-22 11:21:49 +0100

    feat: add confirmation modal component

---
+++ src/presentation/components/confirmation-modal/confirmation-modal.tsx
@@ -0,0 +1,59 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  Button,
+  Flex,
+  Spinner
+} from '@chakra-ui/react';
+
+interface IProps {
+  onClose: () => void;
+  isOpen: boolean;
+  confirm: () => void;
+  loading: boolean;
+}
+
+const ConfirmationModal = ({ onClose, isOpen, confirm, loading }: IProps): JSX.Element => {
+  const handleClick = (): void => {
+    confirm();
+  };
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          {loading ? (
+            <Flex alignContent="center" justifyContent="center" padding="10">
+              <Spinner data-testid="loading-spinner" size="xl" />
+            </Flex>
+          ) : (
+            <>
+              <ModalHeader data-testid="modal-header">Are you sure?</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Text>{"Once it's done it can't be undone!"}</Text>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button data-testid="cancel-button" size="sm" colorScheme="red" mr={3} onClick={onClose}>
+                  No
+                </Button>
+                <Button data-testid="confirm-button" size="sm" colorScheme="green" onClick={handleClick}>
+                  Yes
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ConfirmationModal;
