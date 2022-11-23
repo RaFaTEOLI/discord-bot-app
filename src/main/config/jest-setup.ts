198bf912d4df2ac4f92190c48d11cca9f4d4f3a8
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-23 15:32:57 +0100

    chore: add resizeObserver mock to fix test

--- src/main/config/jest-setup.ts
+++ src/main/config/jest-setup.ts
@@ -1,4 +1,10 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn()
+}));
+
 global.React = React; // this also works for other globally available libraries
