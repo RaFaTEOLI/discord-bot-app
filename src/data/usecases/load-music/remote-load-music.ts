632f9cc2a8d6824deb2260d239aa35db52bc3c92
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-24 16:17:54 +0100

    feat: ensure RemoteLoadMusic will fetch thumbnail from Spotify

--- src/data/usecases/load-music/remote-load-music.ts
+++ src/data/usecases/load-music/remote-load-music.ts
@@ -1,17 +1,42 @@
 import { HttpClient, HttpStatusCode } from '@/data/protocols/http';
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
-import { MusicModel } from '@/domain/models';
+import { MusicModel, SpotifySearchModel } from '@/domain/models';
 import { LoadMusic } from '@/domain/usecases';
 
 export class RemoteLoadMusic implements LoadMusic {
-  constructor(private readonly url: string, private readonly httpClient: HttpClient<RemoteLoadMusic.Model>) {}
+  constructor(
+    private readonly url: string,
+    private readonly httpClient: HttpClient<RemoteLoadMusic.Model>,
+    private readonly spotifyUrl: string,
+    private readonly spotifyHttpClient: HttpClient<SpotifySearchModel>
+  ) {}
 
   async load(): Promise<LoadMusic.Model> {
     // TODO: integrate with spotify, when music is fetched from our db, search it on spotify to obtain thumbnail and song duration
     const httpResponse = await this.httpClient.request({ url: this.url, method: 'get' });
+    let music = httpResponse.body;
+
+    if (music?.name) {
+      const searchResult = await this.spotifyHttpClient.request({
+        url: this.spotifyUrl,
+        method: 'get',
+        params: {
+          q: music.name,
+          type: 'track,artist',
+          market: 'US',
+          limit: 1
+        }
+      });
+
+      music = {
+        ...music,
+        thumbnail: searchResult.body?.tracks.items[0].album.images[0].url
+      };
+    }
+
     switch (httpResponse.statusCode) {
       case HttpStatusCode.success:
-        return httpResponse.body as LoadMusic.Model;
+        return music as LoadMusic.Model;
       case HttpStatusCode.noContent:
         return null;
       case HttpStatusCode.forbidden:
--- src/domain/models/music-model.ts
+++ src/domain/models/music-model.ts
@@ -2,4 +2,36 @@ export type MusicModel = {
   id: string;
   name: string;
   startedAt: number;
+  thumbnail?: string | null;
+};
+
+export type SpotifySearchModel = {
+  artists: {
+    items: [
+      {
+        images: [
+          {
+            height: string | null;
+            url: string | null;
+            width: string | null;
+          }
+        ];
+      }
+    ];
+  };
+  tracks: {
+    items: [
+      {
+        album: {
+          images: [
+            {
+              height: string | null;
+              url: string | null;
+              width: string | null;
+            }
+          ];
+        };
+      }
+    ];
+  };
 };
