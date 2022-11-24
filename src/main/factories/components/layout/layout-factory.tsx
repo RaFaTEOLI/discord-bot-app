23d615cf1ee9c5d1c95c0738591605014a6289c7
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-24 15:36:10 +0100

    feat: add RemoteLoadMusic to LayoutFactory

--- src/main/factories/components/layout/layout-factory.tsx
+++ src/main/factories/components/layout/layout-factory.tsx
@@ -1,11 +1,12 @@
 import { Layout } from '@/presentation/components';
-import { makeRemoteLoadUser, makeRemoteSpotifyAuthorize } from '@/main/factories/usecases';
+import { makeRemoteLoadMusic, makeRemoteLoadUser, makeRemoteSpotifyAuthorize } from '@/main/factories/usecases';
 
 export const LayoutFactory = (): JSX.Element => {
   return (
     <Layout
       loadUser={makeRemoteLoadUser()}
       spotifyAuthorize={makeRemoteSpotifyAuthorize(process.env.VITE_SPOTIFY_LOGIN_REDIRECT_URI as string)}
+      loadMusic={makeRemoteLoadMusic()}
     />
   );
 };
