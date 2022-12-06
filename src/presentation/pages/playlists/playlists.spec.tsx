4eb797bfda0bcb8d116b129426182a76d2157c84
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-12-06 14:21:22 +0100

    test: add base playlists page

---
+++ src/presentation/pages/playlists/playlists.spec.tsx
@@ -0,0 +1,27 @@
+import { renderWithHistory } from '@/presentation/mocks';
+import { screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import Playlists from './playlists';
+
+const history = createMemoryHistory({ initialEntries: ['/'] });
+const makeSut = (): void => {
+  renderWithHistory({
+    history,
+    useAct: true,
+    Page: () => Playlists()
+  });
+};
+
+describe('Playlists Component', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  test('should have Playlists page content', () => {
+    makeSut();
+    const pageContent = screen.getByRole('heading', {
+      name: 'Playlists'
+    });
+    expect(pageContent).toBeInTheDocument();
+  });
+});
