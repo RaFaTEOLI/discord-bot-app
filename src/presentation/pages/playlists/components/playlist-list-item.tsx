a6795b72a0e4cffb870b2a070a9e8cb70ec05280
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-12-08 16:06:36 +0100

    feat: ensure playlist card will show play icon on hover

--- src/presentation/pages/playlists/components/playlist-list-item.tsx
+++ src/presentation/pages/playlists/components/playlist-list-item.tsx
@@ -1,6 +1,8 @@
 import { SpotifyPlaylistModel } from '@/domain/models';
-import { Box, Flex, IconButton, Tooltip } from '@chakra-ui/react';
-import { HiEye, HiPlay } from 'react-icons/hi2';
+import { Box, Flex, IconButton, Image, useColorModeValue } from '@chakra-ui/react';
+import { MouseEvent, useState } from 'react';
+import { HiPlay } from 'react-icons/hi2';
+import { motion } from 'framer-motion';
 
 type Props = {
   playlists: SpotifyPlaylistModel[];
@@ -9,45 +11,78 @@ type Props = {
 };
 
 export default function PlaylistListItem({ playlists, handleView, handlePlay }: Props): JSX.Element {
+  const color = useColorModeValue('gray.50', 'gray.900');
+  const hoverColor = useColorModeValue('gray.100', 'gray.700');
+  const [currentHover, setCurrentHover] = useState<string | null>(null);
+
+  const handleMouseEnter = (playlistId: string): void => {
+    setCurrentHover(playlistId);
+  };
+
+  const handleMouseLeave = (): void => {
+    setCurrentHover(null);
+  };
+
+  const handleClickPlay = (event: MouseEvent<HTMLButtonElement>, url: string): void => {
+    event.stopPropagation();
+    handlePlay(url);
+  };
+
+  const animate = {
+    y: -12,
+    opacity: 1
+  };
+
   return (
     <Flex direction={['column', 'row']} flexWrap="wrap" gap={5} data-testid="playlists-list">
       {playlists.map(playlist => (
-        <Box key={playlist.id} w={[230, 280]} borderWidth="1px" borderRadius="lg" overflow="hidden">
+        <Box
+          bgColor={currentHover === playlist.id ? hoverColor : color}
+          key={playlist.id}
+          w={[230, 280]}
+          h={[250, 370]}
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          cursor="pointer"
+          onMouseEnter={() => handleMouseEnter(playlist.id)}
+          onMouseLeave={handleMouseLeave}
+          className="playlist-view-button"
+          onClick={() => handleView(playlist.id)}
+        >
           <Box p="6">
-            <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" noOfLines={1} className="playlist-name">
+            <Box display="flex" justifyContent="center" position="relative">
+              <Image width={225} height={225} src={playlist.images[0].url} alt={`Album ${playlist.name}`} />
+
+              <motion.div
+                initial={{ opacity: 0 }}
+                transition={{ ease: 'easeOut', duration: 0.5 }}
+                {...(currentHover === playlist.id && { animate })}
+              >
+                <IconButton
+                  display={currentHover === playlist.id ? 'flex' : 'none'}
+                  position="absolute"
+                  right={3}
+                  bottom={0}
+                  className="playlist-play-button"
+                  variant="solid"
+                  borderRadius={50}
+                  size="lg"
+                  colorScheme="green"
+                  aria-label="Play Playlist"
+                  icon={<HiPlay />}
+                  onClick={event => handleClickPlay(event, playlist.external_urls.spotify)}
+                />
+              </motion.div>
+            </Box>
+
+            <Box mt="3" fontWeight="semibold" as="h4" lineHeight="tight" noOfLines={1} className="playlist-name">
               {playlist.name}
             </Box>
 
             <Flex flexDir="column" justifyContent="space-between">
-              <Box className="playlist-description" h="3rem">
+              <Box mt={2} className="playlist-description" h="2.5rem" noOfLines={2} fontSize={14}>
                 {playlist.description}
-              </Box>
-
-              <Box display="flex" mt="2" alignItems="center" justifyContent="space-between">
-                <Box display="flex" alignItems="center">
-                  <Tooltip label="View Playlist">
-                    <IconButton
-                      className="playlist-view-button"
-                      variant="outline"
-                      size="sm"
-                      colorScheme="blue"
-                      aria-label="View Playlist"
-                      icon={<HiEye />}
-                      onClick={() => handleView(playlist.id)}
-                    />
-                  </Tooltip>
-                  <Tooltip label="Play Playlist">
-                    <IconButton
-                      className="playlist-play-button"
-                      variant="outline"
-                      size="sm"
-                      colorScheme="green"
-                      aria-label="Play Playlist"
-                      icon={<HiPlay />}
-                      onClick={() => handlePlay(playlist.external_urls.spotify)}
-                    />
-                  </Tooltip>
-                </Box>
               </Box>
             </Flex>
           </Box>
