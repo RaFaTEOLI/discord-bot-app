a84d7d1923a3bfa65dc5e058a88928d10d427e0a
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-12-08 11:07:37 +0100

    feat: ensure Playlist page will call LoadUserPlaylist

--- src/presentation/pages/playlists/playlists.tsx
+++ src/presentation/pages/playlists/playlists.tsx
@@ -1,5 +1,17 @@
+import { LoadUserPlaylists } from '@/domain/usecases';
 import { Heading } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
-export default function Home(): JSX.Element {
+type Props = {
+  loadUserPlaylists: LoadUserPlaylists;
+};
+
+export default function Playlists({ loadUserPlaylists }: Props): JSX.Element {
+  useEffect(() => {
+    (async () => {
+      await loadUserPlaylists.all();
+    })();
+  }, []);
+
   return <Heading>Playlists</Heading>;
 }
