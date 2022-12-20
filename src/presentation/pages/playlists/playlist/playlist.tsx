6d34aa456eb253ac6b45413aa338dcc892f361b9
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-12-20 10:02:53 +0100

    feat: add song grid to playlist page

--- src/presentation/pages/playlists/playlist/playlist.tsx
+++ src/presentation/pages/playlists/playlist/playlist.tsx
@@ -1,5 +1,5 @@
 import { Loading } from '@/presentation/components';
-import { Avatar, Flex, HStack, IconButton, Image, Text, useColorModeValue } from '@chakra-ui/react';
+import { Avatar, Flex, HStack, IconButton, Image, Text, useColorModeValue, VStack, Grid, GridItem } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { HiPlay } from 'react-icons/hi2';
 import { useRecoilState, useResetRecoilState } from 'recoil';
@@ -7,6 +7,8 @@ import { InputFilter, userPlaylistState } from './components';
 
 export default function Playlist(): JSX.Element {
   const color = useColorModeValue('gray.600', 'gray.400');
+  const trackColor = useColorModeValue('gray.50', 'gray.900');
+  const trackHoverColor = useColorModeValue('gray.100', 'gray.700');
   const resetUserPlaylistState = useResetRecoilState(userPlaylistState);
   const [state, setState] = useRecoilState(userPlaylistState);
 
@@ -69,46 +71,111 @@ export default function Playlist(): JSX.Element {
           <Loading />
         </Flex>
       ) : (
-        <>
-          <Image
-            boxSize="200px"
-            src="https://wrapped-images.spotifycdn.com/image/yts-2022/large/your-top-songs-2022_large_pt-BR.jpg"
-            alt="Dan Abramov"
-          />
-          <Flex flexDir="column" h="200px" justifyContent="space-between" w="100%">
-            <Text>Public Playlist</Text>
-            <Text fontSize={50} fontWeight={700}>
-              {state.playlist.name}
-            </Text>
-            <Text color={color} fontSize="sm">
-              {state.playlist.description}
-            </Text>
-            <Flex justifyContent="space-between" w="100%">
-              <HStack>
-                <Avatar
-                  size="sm"
-                  name="Rafael Tessarollo"
-                  src="https://scontent-ams2-1.xx.fbcdn.net/v/t1.18169-1/18199154_1201124763332887_8123261132169986051_n.jpg?stp=dst-jpg_p320x320&_nc_cat=100&ccb=1-7&_nc_sid=0c64ff&_nc_ohc=Sa3dIE_3nZQAX-bRbDz&_nc_ht=scontent-ams2-1.xx&edm=AP4hL3IEAAAA&oh=00_AfCG0nZkDks6gW8i6aexxX7Xvk8PlZgJcCQi_kVS_p-Vkw&oe=63BA4985"
-                />
-                <Text>
-                  <b>{state.playlist.owner.display_name}</b> • 99 Songs
-                </Text>
-              </HStack>
-              <HStack>
-                <InputFilter borderRightRadius={5} />
-                <IconButton
-                  className="playlist-play-button"
-                  variant="solid"
-                  borderRadius={50}
-                  size="lg"
-                  colorScheme="green"
-                  aria-label="Play Playlist"
-                  icon={<HiPlay />}
-                />
-              </HStack>
+        <VStack w="100%">
+          <Flex w="100%" flexDirection={['column', 'column', 'column', 'row']} gap={3} data-testid="playlist-header">
+            <Image boxSize="200px" src={state.playlist.images[0].url} alt={state.playlist.name} />
+            <Flex flexDir="column" h="200px" justifyContent="space-between" w="100%">
+              <Text>Public Playlist</Text>
+              <Text fontSize={50} fontWeight={700}>
+                {state.playlist.name}
+              </Text>
+              <Text color={color} fontSize="sm">
+                {state.playlist.description}
+              </Text>
+              <Flex justifyContent="space-between" w="100%" flexDirection={['column', 'column', 'column', 'row']}>
+                <HStack>
+                  <Avatar
+                    size="sm"
+                    name="Rafael Tessarollo"
+                    src="https://scontent-ams2-1.xx.fbcdn.net/v/t1.18169-1/18199154_1201124763332887_8123261132169986051_n.jpg?stp=dst-jpg_p320x320&_nc_cat=100&ccb=1-7&_nc_sid=0c64ff&_nc_ohc=Sa3dIE_3nZQAX-bRbDz&_nc_ht=scontent-ams2-1.xx&edm=AP4hL3IEAAAA&oh=00_AfCG0nZkDks6gW8i6aexxX7Xvk8PlZgJcCQi_kVS_p-Vkw&oe=63BA4985"
+                  />
+                  <Text>
+                    <b>{state.playlist.owner.display_name}</b> • 99 Songs
+                  </Text>
+                </HStack>
+                <HStack>
+                  <InputFilter borderRightRadius={5} />
+                  <IconButton
+                    className="playlist-play-button"
+                    variant="solid"
+                    borderRadius={50}
+                    size="lg"
+                    colorScheme="green"
+                    aria-label="Play Playlist"
+                    icon={<HiPlay />}
+                  />
+                </HStack>
+              </Flex>
             </Flex>
           </Flex>
-        </>
+          <VStack w="100%" justifyContent="flex-start">
+            <Grid
+              mt={5}
+              borderRadius={5}
+              cursor="pointer"
+              w="100%"
+              templateColumns="repeat(5, 1fr)"
+              alignItems="center"
+              gap={6}
+            >
+              <GridItem w="100%">
+                <Text>Title</Text>
+              </GridItem>
+              <GridItem w="100%">
+                <Text>Album</Text>
+              </GridItem>
+              <GridItem w="100%">
+                <Text>Added at</Text>
+              </GridItem>
+              <GridItem w="100%">
+                <Text>Duration</Text>
+              </GridItem>
+            </Grid>
+
+            <Grid
+              borderRadius={5}
+              cursor="pointer"
+              w="100%"
+              bgColor={trackColor}
+              _hover={{ bg: trackHoverColor }}
+              templateColumns="repeat(5, 1fr)"
+              alignItems="center"
+              gap={6}
+            >
+              <GridItem w="100%">
+                <Flex alignItems="center" gap={3}>
+                  <Image boxSize="70px" src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+                  <Flex flexDirection="column">
+                    <Text fontWeight={600}>Numb</Text>
+                    <Text fontWeight={300}>Linkin Park</Text>
+                  </Flex>
+                </Flex>
+              </GridItem>
+              <GridItem w="100%">
+                <Text fontWeight={300}>Meteora</Text>
+              </GridItem>
+              <GridItem w="100%">
+                <Text fontWeight={300}>15 de set. de 2020</Text>
+              </GridItem>
+              <GridItem w="100%">
+                <Text fontWeight={300}>3:06</Text>
+              </GridItem>
+              <GridItem w="100%">
+                <Flex justifyContent="flex-end" pr={5}>
+                  <IconButton
+                    className="song-play-button"
+                    variant="solid"
+                    borderRadius={50}
+                    size="lg"
+                    colorScheme="green"
+                    aria-label="Play Song"
+                    icon={<HiPlay />}
+                  />
+                </Flex>
+              </GridItem>
+            </Grid>
+          </VStack>
+        </VStack>
       )}
     </Flex>
   );
