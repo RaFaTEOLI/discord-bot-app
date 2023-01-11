722eb8654924e44443f917a3c9dc1d8143f8e654
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-01-11 17:35:17 +0100

    test: ensure Playlist will render error on AccessDeniedError on LoadPlaylistTracks

--- src/presentation/pages/playlists/playlist/playlist.spec.tsx
+++ src/presentation/pages/playlists/playlist/playlist.spec.tsx
@@ -1,9 +1,10 @@
-import { UnexpectedError } from '@/domain/errors';
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
 import { LoadPlaylistTracksSpy } from '@/domain/mocks';
 import { AccountModel } from '@/domain/models';
 import { renderWithHistory } from '@/presentation/mocks';
 import { screen, waitFor } from '@testing-library/react';
 import { createMemoryHistory, MemoryHistory } from 'history';
+import { setTimeout } from 'timers/promises';
 import Playlist from './playlist';
 
 type SutTypes = {
@@ -39,13 +40,23 @@ describe('Playlist Component', () => {
   });
 
   test('should render error on UnexpectedError on LoadPlaylistTracks', async () => {
-    const loadPlaylistTracks = new LoadPlaylistTracksSpy();
+    const loadPlaylistTracksSpy = new LoadPlaylistTracksSpy();
     const error = new UnexpectedError();
-    jest.spyOn(loadPlaylistTracks, 'load').mockRejectedValueOnce(error);
-    makeSut(loadPlaylistTracks);
+    jest.spyOn(loadPlaylistTracksSpy, 'load').mockRejectedValueOnce(error);
+    makeSut(loadPlaylistTracksSpy);
     await waitFor(() => screen.getByTestId('error'));
     expect(screen.queryByTestId('playlists-list')).not.toBeInTheDocument();
     const errorWrap = await screen.findByTestId('error');
     expect(errorWrap).toHaveTextContent(error.message);
+  });
+
+  test('should render error on AccessDeniedError on LoadPlaylistTracks', async () => {
+    const loadPlaylistTracksSpy = new LoadPlaylistTracksSpy();
+    jest.spyOn(loadPlaylistTracksSpy, 'load').mockRejectedValueOnce(new AccessDeniedError());
+    const { setCurrentAccountMock, history } = makeSut(loadPlaylistTracksSpy);
+    await waitFor(() => screen.getByTestId('playlist-container'));
+    await setTimeout(500);
+    expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined);
+    expect(history.location.pathname).toBe('/login');
   });
 });
