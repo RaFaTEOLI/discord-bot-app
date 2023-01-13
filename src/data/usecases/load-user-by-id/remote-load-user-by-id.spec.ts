635bc97cd0c1de3a7b00c6187acb31cce7bc5675
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-01-13 13:23:02 +0100

    test: ensure RemoteLoadUserById will return a SpotifyUser

---
+++ src/data/usecases/load-user-by-id/remote-load-user-by-id.spec.ts
@@ -0,0 +1,89 @@
+import { HttpClientSpy } from '@/data/mocks';
+import { RemoteLoadUserById } from '@/data/usecases';
+import { HttpStatusCode } from '@/data/protocols/http';
+import { AccessDeniedError, AccessTokenExpiredError, UnexpectedError } from '@/domain/errors';
+import { faker } from '@faker-js/faker';
+import { mockSpotifyUserById } from '@/domain/mocks';
+
+type SutTypes = {
+  sut: RemoteLoadUserById;
+  httpClientSpy: HttpClientSpy;
+};
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpClientSpy();
+  const sut = new RemoteLoadUserById(url, httpClientSpy);
+
+  return {
+    sut,
+    httpClientSpy
+  };
+};
+
+describe('RemoteLoadUserById', () => {
+  test('should call HttpClient with correct URL, Method and Headers', async () => {
+    const url = faker.internet.url();
+    const { sut, httpClientSpy } = makeSut(url);
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.success,
+      body: mockSpotifyUserById()
+    };
+    const id = faker.datatype.uuid();
+    await sut.loadById(id);
+    expect(httpClientSpy.url).toBe(`${url}/${id}`);
+    expect(httpClientSpy.method).toBe('get');
+    expect(httpClientSpy.headers).toEqual({ 'Content-Type': 'application/json' });
+  });
+
+  test('should throw AccessDeniedError if HttpClient returns 403', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    };
+    const promise = sut.loadById(faker.datatype.uuid());
+    await expect(promise).rejects.toThrow(new AccessDeniedError());
+  });
+
+  test('should throw AccessTokenExpiredError if HttpClient returns 401', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized
+    };
+    const promise = sut.loadById(faker.datatype.uuid());
+    await expect(promise).rejects.toThrow(new AccessTokenExpiredError());
+  });
+
+  test('should throw UnexpectedError if HttpClient returns 404', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    };
+    const promise = sut.loadById(faker.datatype.uuid());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('should throw UnexpectedError if HttpClient returns 500', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    };
+    const promise = sut.loadById(faker.datatype.uuid());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('should return a SpotifyUser if HttpClient returns 200', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    const httpResult = mockSpotifyUserById();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.success,
+      body: httpResult
+    };
+    const spotifyUser = await sut.loadById(faker.datatype.uuid());
+    expect(spotifyUser).toEqual({
+      display_name: httpResult.display_name,
+      id: httpResult.id,
+      external_urls: httpResult.external_urls,
+      images: httpResult.images
+    });
+  });
+});
--- src/domain/mocks/mock-spotify-user.ts
+++ src/domain/mocks/mock-spotify-user.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { LoadUser } from '@/domain/usecases';
+import { LoadUser, LoadUserById } from '@/domain/usecases';
 
 export const mockSpotifyUser = (): LoadUser.Model => ({
   display_name: faker.name.fullName(),
@@ -21,11 +21,38 @@ export const mockSpotifyUser = (): LoadUser.Model => ({
   product: 'premium'
 });
 
+export const mockSpotifyUserById = (): LoadUserById.Model => ({
+  display_name: faker.name.fullName(),
+  id: faker.datatype.uuid(),
+  external_urls: [
+    {
+      spotify: faker.internet.url()
+    }
+  ],
+  images: [
+    {
+      height: null,
+      url: faker.internet.url(),
+      width: null
+    }
+  ]
+});
+
 export class LoadUserSpy implements LoadUser {
   spotifyUser = mockSpotifyUser();
   callsCount = 0;
 
   async load(): Promise<LoadUser.Model> {
+    this.callsCount++;
+    return Promise.resolve(this.spotifyUser);
+  }
+}
+
+export class LoadUserByIdSpy implements LoadUserById {
+  spotifyUser = mockSpotifyUserById();
+  callsCount = 0;
+
+  async loadById(id: string): Promise<LoadUserById.Model> {
     this.callsCount++;
     return Promise.resolve(this.spotifyUser);
   }
