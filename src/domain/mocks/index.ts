f4fcf811f630007eaae0a8b87f1b3de236328ce4
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-06-19 11:15:51 -0300

    test: add SpotifyRefreshToken mock

--- src/domain/mocks/index.ts
+++ src/domain/mocks/index.ts
@@ -17,3 +17,4 @@ export * from './mock-discord-access';
 export * from './mock-discord-request-token';
 export * from './mock-discord-load-user';
 export * from './mock-user';
+export * from './mock-spotify-refresh-token';
---
+++ src/domain/mocks/mock-spotify-refresh-token.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker';
+import { SpotifyRefreshToken } from '@/domain/usecases';
+
+export const mockSpotifyRefreshTokenParams = (): SpotifyRefreshToken.Params => {
+  return {
+    refreshToken: faker.datatype.uuid(),
+    encodedAuthorization: faker.datatype.uuid()
+  };
+};
+
+export class SpotifyRefreshTokenSpy implements SpotifyRefreshToken {
+  url = faker.internet.url();
+  spotifySettings = mockSpotifyRefreshTokenParams();
+  spotifyClientId = faker.datatype.uuid();
+  access = { accessToken: faker.datatype.uuid() };
+  callsCount = 0;
+
+  async refresh(): Promise<SpotifyRefreshToken.Model> {
+    this.callsCount++;
+    return Promise.resolve(this.access);
+  }
+}
