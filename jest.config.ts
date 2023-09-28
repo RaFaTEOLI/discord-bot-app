255bafb059d90abe133697f1e3ac0bf5bee00002
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-09-28 14:09:42 -0300

    chore: add @swc/jest

--- jest.config.ts
+++ jest.config.ts
@@ -20,6 +20,9 @@ export default {
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest'
+  },
   moduleNameMapper: {
     '\\.(css|sass|scss)$': 'identity-obj-proxy',
     '@/(.*)': '<rootDir>/src/$1'
