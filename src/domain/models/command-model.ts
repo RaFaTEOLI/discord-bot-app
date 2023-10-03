2457815d14a827a51e34a16d847060d00872be2a
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-10-03 11:51:36 -0300

    feat: add new properties to CommandModal to match discord api

--- src/domain/models/command-model.ts
+++ src/domain/models/command-model.ts
@@ -1,3 +1,17 @@
+export enum CommandOptionType {
+  SUB_COMMAND = 1,
+  SUB_COMMAND_GROUP = 2,
+  STRING = 3,
+  INTEGER = 4, // Any integer between -2^53 and 2^53
+  BOOLEAN = 5,
+  USER = 6,
+  CHANNEL = 7, // Includes all channel types + categories
+  ROLE = 8,
+  MENTIONABLE = 9, // Includes users and roles
+  NUMBER = 10, // Any double between -2^53 and 2^53
+  ATTACHMENT = 11 // Attachment object
+}
+
 export type CommandModel = {
   id: string;
   command: string;
@@ -5,4 +19,14 @@ export type CommandModel = {
   type: string;
   dispatcher: string;
   response: string;
+  options?: Array<{
+    name: string;
+    description: string;
+    type: CommandOptionType;
+    required: boolean;
+    choices?: Array<{
+      name: string;
+      value: string;
+    }>;
+  }>;
 };
