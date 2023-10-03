daf25aceafecf43ff9eb28f7cad9465afc5e88fe
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-10-03 14:21:03 -0300

    test: ensure a command option will be added on add

---
+++ src/presentation/pages/commands/command/command.spec.tsx
@@ -0,0 +1,83 @@
+import { renderWithHistory } from '@/presentation/mocks';
+import { screen, waitFor } from '@testing-library/react';
+import { createMemoryHistory, MemoryHistory } from 'history';
+import Command from './command';
+import { AccountModel } from '@/domain/models';
+import { SaveCommandSpy, LoadCommandByIdSpy } from '@/domain/mocks';
+import { faker } from '@faker-js/faker';
+import userEvent from '@testing-library/user-event';
+
+const mockToast = jest.fn();
+jest.mock('@chakra-ui/react', () => {
+  const originalModule = jest.requireActual('@chakra-ui/react');
+  return {
+    __esModule: true,
+    ...originalModule,
+    useToast: jest.fn().mockImplementation(() => mockToast)
+  };
+});
+
+type SutTypes = {
+  commandId: string;
+  loadCommandByIdSpy: LoadCommandByIdSpy;
+  saveCommandSpy: SaveCommandSpy;
+  history: MemoryHistory;
+  setCurrentAccountMock: (account: AccountModel) => void;
+};
+
+const history = createMemoryHistory({ initialEntries: ['/commands/1'] });
+const makeSut = (
+  commandId = faker.datatype.uuid(),
+  loadCommandByIdSpy = new LoadCommandByIdSpy(),
+  saveCommandSpy = new SaveCommandSpy(),
+  adminUser = false
+): SutTypes => {
+  const { setCurrentAccountMock } = renderWithHistory({
+    history,
+    useAct: true,
+    adminUser,
+    Page: () =>
+      Command({
+        commandId,
+        loadCommandById: loadCommandByIdSpy,
+        saveCommand: saveCommandSpy
+      })
+  });
+  return {
+    commandId,
+    loadCommandByIdSpy,
+    saveCommandSpy,
+    history,
+    setCurrentAccountMock
+  };
+};
+
+describe('Command Component', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  test('should have command page content', () => {
+    makeSut();
+    const pageContent = screen.getByRole('heading', {
+      name: 'Command'
+    });
+    expect(pageContent).toBeInTheDocument();
+  });
+
+  test('should call LoadCommandById', async () => {
+    const { loadCommandByIdSpy, commandId } = makeSut();
+    await waitFor(() => screen.getByTestId('command-content'));
+    expect(loadCommandByIdSpy.callsCount).toBe(1);
+    expect(loadCommandByIdSpy.commandId).toBe(commandId);
+  });
+
+  test('should add Command Option on option add', async () => {
+    makeSut();
+    await waitFor(() => screen.getByTestId('command-content'));
+    await userEvent.click(screen.getByTestId('add-option'));
+    const optionsList = await screen.findByTestId('options-list');
+    await waitFor(() => optionsList);
+    expect(optionsList.children).toHaveLength(1);
+  });
+});
