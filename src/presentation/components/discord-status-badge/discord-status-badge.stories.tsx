b2ed5ca8c21d0ce4c37bc93eb452832c854bbcab
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-01-29 18:47:09 -0300

    docs: add discord-status-badge story

---
+++ src/presentation/components/discord-status-badge/discord-status-badge.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from '@storybook/react';
+import DiscordStatusBadge, { Props } from './discord-status-badge';
+import Wrapper from '../story-wrapper/chakra-story-wrapper';
+import { CommandDiscordStatus } from '@/domain/models';
+
+export default {
+  title: 'Presentation/Components/DiscordStatusBadge',
+  component: DiscordStatusBadge,
+  decorators: [
+    Story => {
+      return <Wrapper>{Story()}</Wrapper>;
+    }
+  ]
+} as Meta<Props>;
+
+export const Default: StoryObj<Props> = {
+  args: {
+    value: null
+  }
+};
+
+export const Sent: StoryObj<Props> = {
+  args: {
+    value: CommandDiscordStatus.SENT
+  }
+};
+
+export const Received: StoryObj<Props> = {
+  args: {
+    value: CommandDiscordStatus.RECEIVED
+  }
+};
+
+export const Failed: StoryObj<Props> = {
+  args: {
+    value: CommandDiscordStatus.FAILED
+  }
+};
