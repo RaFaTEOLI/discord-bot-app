1a39a9045f94925c3d9a8cac282cdf0886208d96
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-01-30 09:41:41 -0300

    feat: ensure CurrentAccountAdapter will encrypt and decrypt account

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,10 +1,21 @@
 import { AccountModel } from '@/domain/models';
 import { makeLocalStorageAdapter } from '@/main/factories/cache';
+import { makeCryptoJSAdapter } from '@/main/factories/cryptography';
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-  makeLocalStorageAdapter().set('account', account);
+  let encryptedAccount = '';
+  if (account) {
+    encryptedAccount = makeCryptoJSAdapter().encrypt(JSON.stringify(account));
+  }
+  makeLocalStorageAdapter().set(process.env.VITE_LOCAL_STORAGE_ACCOUNT_IDENTIFIER as string, encryptedAccount);
+  makeLocalStorageAdapter().set(process.env.VITE_LOCAL_STORAGE_TOKEN_IDENTIFIER as string, account?.accessToken);
 };
 
 export const getCurrentAccountAdapter = (): AccountModel => {
-  return makeLocalStorageAdapter().get('account');
+  const encryptedAccount = makeLocalStorageAdapter().get(process.env.VITE_LOCAL_STORAGE_ACCOUNT_IDENTIFIER as string);
+  if (encryptedAccount) {
+    const decryptedAccount = makeCryptoJSAdapter().decrypt(encryptedAccount);
+    return JSON.parse(decryptedAccount);
+  }
+  return undefined as unknown as AccountModel;
 };
