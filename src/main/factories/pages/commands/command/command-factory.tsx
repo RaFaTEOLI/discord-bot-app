e2d91a9b260a94e50277dff0fa73652b07635e24
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-01-30 18:11:18 -0300

    feat: add socketClient to command factory

--- src/main/factories/pages/commands/command/command-factory.tsx
+++ src/main/factories/pages/commands/command/command-factory.tsx
@@ -1,3 +1,4 @@
+import { makeSocketClient } from '@/main/factories/http';
 import { makeRemoteLoadCommandById, makeRemoteSaveCommand } from '@/main/factories/usecases';
 import Command from '@/presentation/pages/commands/command/command';
 import { useParams } from 'react-router';
@@ -5,6 +6,11 @@ import { useParams } from 'react-router';
 export const CommandFactory = (): JSX.Element => {
   const { id } = useParams<string>();
   return (
-    <Command commandId={id as string} loadCommandById={makeRemoteLoadCommandById()} saveCommand={makeRemoteSaveCommand()} />
+    <Command
+      commandId={id as string}
+      loadCommandById={makeRemoteLoadCommandById()}
+      saveCommand={makeRemoteSaveCommand()}
+      socketClient={makeSocketClient()}
+    />
   );
 };
