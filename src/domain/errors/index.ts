da164b78958ca2f95ae729dde2f890a0b96ae917
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-02-14 16:50:09 -0300

    feat: ensure RemoteSaveCommand will return CommandAlreadyCreatedError if HttpClient returns 400 with CommandAlreadyCreatedError message

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -4,3 +4,4 @@ export * from './unexpected-error';
 export * from './access-denied-error';
 export * from './access-token-expired-error';
 export * from './forbidden-error';
+export * from './command-already-created-error';
