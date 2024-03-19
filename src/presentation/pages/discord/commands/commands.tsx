08718da425952c0fc954ccf6ae55c94c44e922f6
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-03-19 17:27:12 -0300

    feat: ensure DiscordCommands page will show commands

--- src/presentation/pages/discord/commands/commands.tsx
+++ src/presentation/pages/discord/commands/commands.tsx
@@ -1,21 +1,35 @@
 import { LoadDiscordCommands } from '@/domain/usecases';
 import { Content } from '@/presentation/components';
 import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { discordCommandsState } from './components';
 
 type Props = {
   loadDiscordCommands: LoadDiscordCommands;
 };
 
 export default function Commands({ loadDiscordCommands }: Props): JSX.Element {
+  const [state, setState] = useRecoilState(discordCommandsState);
+
   useEffect(() => {
     (async () => {
-      await loadDiscordCommands.all();
+      const response = await loadDiscordCommands.all();
+      setState({ commands: response, isLoading: false });
     })();
   }, [loadDiscordCommands]);
 
   return (
     <Content title="Commands">
-      <p>Commands Page</p>
+      <>
+        <p>Commands Page</p>
+        <div data-testid="discord-commands">
+          {state.commands.map(command => (
+            <div key={command.id} className="command">
+              {command.name}
+            </div>
+          ))}
+        </div>
+      </>
     </Content>
   );
 }
