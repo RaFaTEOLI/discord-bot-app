0556862a9973070e9ace70d166d881461e03c5d5
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-09-12 18:47:33 -0300

    feat: add spotify guest token url retrieval

--- src/main/factories/decorators/spotify-http-client-decorator-factory.ts
+++ src/main/factories/decorators/spotify-http-client-decorator-factory.ts
@@ -2,7 +2,13 @@ import { HttpClient } from '@/data/protocols/http';
 import { SpotifyHttpClientDecorator } from '@/main/decorators';
 import { makeLocalStorageAdapter } from '@/main/factories/cache/local-storage-adapter-factory';
 import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory';
+import { makeApiUrl } from '../http';
 
 export const makeSpotifyHttpClientDecorator = (): HttpClient => {
-  return new SpotifyHttpClientDecorator(makeLocalStorageAdapter(), makeLocalStorageAdapter(), makeAxiosHttpClient());
+  return new SpotifyHttpClientDecorator(
+    makeLocalStorageAdapter(),
+    makeLocalStorageAdapter(),
+    makeAxiosHttpClient(),
+    makeApiUrl('/spotify/guest-token')
+  );
 };
